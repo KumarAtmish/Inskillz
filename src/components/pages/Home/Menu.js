9432113742009ca1f357b9398d2459ed42ca54af
KumarAtmish <atmishkumar@gmail.com>
2021-01-09 19:08:58 +0530

    update

--- src/components/pages/Home/Menu.js
+++ src/components/pages/Home/Menu.js
@@ -4,23 +4,26 @@ import "./Menu.css";
 import logo from "./image/logo.png"
 
 export default function Menu() {
+    const print =()=>{
+      console.log("inprint")
+    }
     return (
         <header className="header">
-      <nav className="navbar navbar-expand-lg p-0">
+      <nav className="navbar navbar-expand-lg fixed-top py-0">
           <div className="container">
              <Link to="/" className="navbar-brand text-uppercase font-weight-bold">
                 <img src={logo} alt="" />
              </Link>
-             <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
+             <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><i class="fa fa-bars"></i></button>
               
               <div id="navbarSupportedContent" className="collapse navbar-collapse">
                   <ul className="navbar-nav ml-auto">
-                      <li className="nav-item active"><Link to="/" className="nav-link text-uppercase font-weight-bold">Home <span className="sr-only">(current)</span></Link></li>
+                      <li className="nav-item active" onClick={()=>print()}><Link to="/" className="nav-link text-uppercase font-weight-bold">Home <span className="sr-only">(current)</span></Link></li>
                       <li className="nav-item"><Link to="/about" className="nav-link text-uppercase font-weight-bold">About</Link></li>
                       <li className="nav-item"><Link to="/courses" className="nav-link text-uppercase font-weight-bold">Courses</Link></li>
                       <li className="nav-item"><Link to="/teachers" className="nav-link text-uppercase font-weight-bold">Teachers</Link></li>
-                      <li className="nav-item"><Link to="/teachers" className="nav-link text-uppercase font-weight-bold">Tech on Inskillz</Link></li>
-                      <li className="nav-item"><Link to="/tech" className="nav-link text-uppercase font-weight-bold">Contact</Link></li>
+                      <li className="nav-item"><Link to="/tech" className="nav-link text-uppercase font-weight-bold">Tech on Inskillz</Link></li>
+                      <li className="nav-item"><Link to="/contact" className="nav-link text-uppercase font-weight-bold">Contact</Link></li>
                   </ul>
               </div>
           </div>
